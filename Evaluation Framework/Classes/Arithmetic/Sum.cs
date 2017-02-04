460a1a4ff470fcd4673c6a2a00193c5029f9f40e
Oren (electricessence) <oren@electrified.net>
2017-02-03 18:16:27 -0800

    Wrapped up reduction code for Sum.

--- Evaluation Framework/Classes/Arithmetic/Sum.cs
+++ Evaluation Framework/Classes/Arithmetic/Sum.cs
@@ -31,6 +31,8 @@ namespace EvaluationFramework.ArithmeticOperators
 		public override IEvaluate<TContext, TResult> Reduction()
 		{
 			var children = new List<IEvaluate<TContext, TResult>>();
+			if (children.Count == 0)
+				return new Constant<TContext, TResult>((TResult)(dynamic)0);
 
 			// Phase 1: Flatten sums of sums.
 			foreach (var child in ChildrenInternal)
@@ -80,12 +82,20 @@ namespace EvaluationFramework.ArithmeticOperators
 				}
 			}
 
-
+			// Phase 5: Replace multipliable products with single merged version.
 			foreach (var p in productsWithConstants
 				.GroupBy(g => g.Item1)
 				.Where(g => g.Count() > 1))
 			{
-				
+				var p1 = p.First();
+				var reduced = p1.Item3;
+				var multiple = p.Select(t=>t.Item2).Sum();
+				foreach (var px in p.Select(t=>t.Item4))
+					children.Remove(px);
+
+				var replacement = new List<IEvaluate<TContext, TResult>>(p.Select(t => t.Item3));
+				replacement.Add(multiple);
+				children.Add(new Product<TContext, TResult>(replacement));
 			}
 
 			if (constants.Any())
@@ -97,124 +107,6 @@ namespace EvaluationFramework.ArithmeticOperators
 			return result.ToStringRepresentation() == result.ToStringRepresentation() ? null : result;
 		}
 
-
-
-
-		protected override void ReduceLoop()
-		{
-			// Collapse sums within sums.
-			var children = GetChildren();
-			if (children.Count == 0) return;
-
-			foreach (var p in children.OfType<SumGene>().ToArray())
-			{
-				var m = p.Multiple;
-				foreach (var s in p)
-				{
-					s.Multiple *= m;
-					children.Add(s);
-				}
-				p.Clear();
-				children.Remove(p);
-			}
-
-			// Flatten negatives...
-			if (Multiple < 0 && children.Any(c => c.Multiple < 0) || Multiple > 0 && children.All(c => c.Multiple < 0))
-			{
-				Multiple *= -1;
-				foreach (var g in children)
-					g.Multiple *= -1;
-			}
-
-			if (children.All(c =>
-			{
-				var cm = c.Multiple;
-				return !double.IsNaN(cm) && !double.IsInfinity(cm);
-			}))
-			{
-				// Pull out multiples.
-				using (var absMultiples = children.Select(c => Math.Abs(c.Multiple)).Where(m => m != 0 && m != 1).Distinct().Memoize())
-				{
-					if (absMultiples.Any())
-					{
-						var max = absMultiples.Min();
-						for (var i = 2; max != 1 && i <= max; i = i.NextPrime())
-						{
-							while (max % i == 0 && children.All(g => g.Multiple % i == 0))
-							{
-								max /= i;
-								Multiple *= i;
-								foreach (var g in children)
-								{
-									g.Multiple /= i;
-								}
-							}
-						}
-
-					}
-				}
-			}
-
-			// Combine any constants.  This is more optimal because we don't neet to query ToStringContents.
-			var constants = children.OfType<ConstantGene>().ToArray();
-			if (constants.Length > 1)
-			{
-				var main = constants.First();
-				foreach (var c in constants.Skip(1))
-				{
-					main.Multiple += c.Multiple;
-					children.Remove(c);
-				}
-			}
-
-			RemoveZeroMultiples();
-
-			// Look for groupings...
-			foreach (var p in children
-				.Where(g => !(g is ConstantGene)) // We just reduced constants above so skip them...
-				.GroupBy(g => g.ToStringUsingMultiple(1))
-				.Where(g => g.Count() > 1))
-			{
-				using (var matches = p.Memoize())
-				{
-					// Take matching groupings and merge them.
-					var main = matches.First();
-					var sum = matches.Sum(s => s.Multiple);
-
-					if (sum == 0)
-						// Remove the gene that would remain with a zero.
-						Remove(main);
-					else
-						main.Multiple = sum;
-
-					// Remove the other genes that are now useless.
-					foreach (var gene in matches.Skip(1))
-						Remove(gene);
-
-					break;
-				}
-			}
-
-			RemoveZeroMultiples();
-		}
-
-		protected override IGene ReplaceWithReduced()
-		{
-			var children = GetChildren();
-			switch (children.Count)
-			{
-				case 0:
-					return new ConstantGene(0);
-				case 1:
-					var c = children.Single();
-					c.Multiple *= Multiple;
-					Remove(c);
-					return c;
-			}
-			if (children.Any(c => double.IsNaN(c.Multiple)))
-				return new ConstantGene(double.NaN);
-			return base.ReplaceWithReduced();
-		}
 	}
 
 	public static class Sum
