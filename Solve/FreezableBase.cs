749214330d5df26fc1a6a6623ad8a6bb4306fddb
electricessence <oren@electrified.net>
2017-10-20 20:37:06 -0700

    Updated freezable

--- Solve/FreezableBase.cs
+++ Solve/FreezableBase.cs
@@ -1,32 +1,28 @@
-﻿/*!
- * @author electricessence / https://github.com/electricessence/
- * Licensing: Apache https://github.com/electricessence/Solve/blob/master/LICENSE.txt
- */
-
-using System;
+﻿using System;
+using System.Threading;
 
 namespace Solve
 {
 
-	public abstract class FreezableBase : IFreeze
+	public abstract class FreezableBase : IFreezable
 	{
 
-		public bool IsReadOnly
-		{
-			get;
-			private set;
-		}
+		int _frozenState = 0;
+		public bool IsReadOnly => _frozenState == 1;
 
+		// It's concevable that mutliple threads could 're-attempt' to freeze a object 'in the wild'.
 		public void Freeze()
 		{
-			OnBeforeFreeze();
-			IsReadOnly = true;
+			if(0==_frozenState && 0==Interlocked.CompareExchange(ref _frozenState, 1, 0))
+			{
+				OnBeforeFreeze(); // Ensure this is only called once.
+			}
 		}
 
 		protected void AssertIsNotFrozen()
 		{
 			if (IsReadOnly)
-				throw new InvalidOperationException("Genome is frozen.");
+				throw new InvalidOperationException("Object is frozen.");
 		}
 
 		protected abstract void OnBeforeFreeze();
--- Solve/IFreezable.cs
+++ Solve/IFreezable.cs
@@ -1,22 +1,18 @@
-﻿/*!
- * @author electricessence / https://github.com/electricessence/
- * Licensing: Apache https://github.com/electricessence/Solve/blob/master/LICENSE.txt
- */
-
-namespace Solve
+﻿namespace Solve
 {
 	/*
      * It's important to have an interface that ensures an object is in flux or won't change.
      * This way we can avoid synchronization woes and use virtually immutable objects instead.
      */
 
-	public interface IFreeze
+	public interface IFreezable
 	{
 		// True if frozen.
 		bool IsReadOnly { get; }
 
 		/*
          * Should prevent further modifications to the genome.
+		 * Needs to be thread safe.
          */
 		void Freeze();
 	}
