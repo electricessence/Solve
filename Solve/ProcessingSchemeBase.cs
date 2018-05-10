e8541e634cda4f8bacbab151020d8a52394b877d
Oren (electricessence) <oren@electrified.net>
2018-05-10 09:30:22 -0700

    Tweaks.

--- Solve/ProcessingSchemeBase.cs
+++ Solve/ProcessingSchemeBase.cs
@@ -19,38 +19,10 @@ namespace Solve
 		bool _isComplete = false;
 		public Task Completion => ChampionAnnouncer.Completion;
 
-		protected abstract Task Process(IGenomeFitness<TGenome, Fitness> next);
+		public abstract Task Post(IGenomeFitness<TGenome, Fitness> next);
 
 		internal bool Announce(IGenomeFitness<TGenome, Fitness> newChampion)
 			=> ChampionAnnouncer.Post(newChampion);
-
-		public DataflowMessageStatus OfferMessage(
-			DataflowMessageHeader messageHeader,
-			IGenomeFitness<TGenome, Fitness> messageValue,
-			ISourceBlock<IGenomeFitness<TGenome, Fitness>> source,
-			bool consumeToAccept)
-		{
-			if (_isComplete) return DataflowMessageStatus.DecliningPermanently;
-			Process(messageValue).Wait();
-			return DataflowMessageStatus.Accepted;
-		}
-
-		public IDisposable LinkTo(
-			ITargetBlock<IGenomeFitness<TGenome, Fitness>> target,
-			DataflowLinkOptions linkOptions)
-			=> _announcerBlock.LinkTo(target, linkOptions);
-
-		public IGenomeFitness<TGenome, Fitness> ConsumeMessage(
-			DataflowMessageHeader messageHeader,
-			ITargetBlock<IGenomeFitness<TGenome, Fitness>> target,
-			out bool messageConsumed)
-			=> _announcerBlock.ConsumeMessage(messageHeader, target, out messageConsumed);
-
-		public bool ReserveMessage(DataflowMessageHeader messageHeader, ITargetBlock<IGenomeFitness<TGenome, Fitness>> target)
-			=> _announcerBlock.ReserveMessage(messageHeader, target);
-
-		public void ReleaseReservation(DataflowMessageHeader messageHeader, ITargetBlock<IGenomeFitness<TGenome, Fitness>> target)
-			=> _announcerBlock.ReleaseReservation(messageHeader, target);
 
 		public void Complete()
 		{
