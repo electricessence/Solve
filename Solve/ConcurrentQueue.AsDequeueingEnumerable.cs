84496e78884c74388ca134a06b487df36c1b00f1
Oren (electricessence) <oren@electrified.net>
2018-06-03 13:15:20 -0700

    Updates.

--- Solve/ConcurrentQueue.AsDequeueingEnumerable.cs
+++ Solve/ConcurrentQueue.AsDequeueingEnumerable.cs
@@ -1,16 +1,14 @@
-﻿using System;
-using System.Collections.Concurrent;
+﻿using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Solve
 {
-    public static class ConcurrentQueueExtensions
-    {
+	public static class ConcurrentQueueExtensions
+	{
 		public static IEnumerable<T> AsDequeueingEnumerable<T>(this ConcurrentQueue<T> source)
 		{
 			while (source.TryDequeue(out T entry))
 				yield return entry;
 		}
-    }
+	}
 }
