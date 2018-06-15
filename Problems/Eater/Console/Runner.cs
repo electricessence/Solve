1a670e6a5f2dbd8f08eed5a3fdec7bcbd1d7718d
Oren (electricessence) <oren@electrified.net>
2018-06-14 22:23:56 -0700

    Added optional constructor.

--- Problems/Eater/Console/Runner.cs
+++ Problems/Eater/Console/Runner.cs
@@ -32,7 +32,7 @@ namespace Eater
 			var emitter = new EaterConsoleEmitter(problem.Samples, _minSamples);
 			//var scheme = new PyramidPipeline<EaterGenome>(factory, 20, 4, 2, 200);
 			//var scheme = new KingOfTheHill<EaterGenome>(factory, 300, _minConvSamples, 5);
-			var scheme = new ClassicProcessingScheme<EaterGenome>(Factory, (100, 50, 2));
+			var scheme = new ClassicProcessingScheme<EaterGenome>(Factory, 200);
 			//var scheme = new KumiteProcessingScheme<EaterGenome>(Factory, 5);
 			//var scheme = new SinglePool<EaterGenome>(factory, 200);
 
