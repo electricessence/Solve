ccc3d8af9cea238352a6d4e49b11fd44e91926c5
Oren (electricessence) <oren@electrified.net>
2018-06-16 17:25:40 -0700

    Migrated champion pool to ProcessingSchemeBase.

--- Solve/ProcessingSchemes/Classic/Classic.cs
+++ Solve/ProcessingSchemes/Classic/Classic.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.Diagnostics.Contracts;
-using System.Linq;
-using System.Threading;
 
 namespace Solve.ProcessingSchemes
 {
@@ -14,8 +11,8 @@ namespace Solve.ProcessingSchemes
 			(ushort First, ushort Minimum, ushort Step) poolSize,
 			ushort maxLevelLosses = 5,
 			ushort maxLossesBeforeElimination = 30,
-			ushort reserveBreederPoolSize = 20)
-			: base(genomeFactory)
+			ushort championPoolSize = 20)
+			: base(genomeFactory, championPoolSize)
 		{
 			if (poolSize.Minimum < 2)
 				throw new ArgumentOutOfRangeException(nameof(poolSize), "Must be at least 2.");
@@ -32,9 +29,6 @@ namespace Solve.ProcessingSchemes
 			PoolSize = poolSize;
 			MaxLevelLosses = maxLevelLosses;
 			MaxLossesBeforeElimination = maxLossesBeforeElimination;
-			FactoryReserve = genomeFactory[2];
-			FactoryReserve.ExternalProducers.Add(ProduceFromReserve);
-			ReserveBreederPoolSize = reserveBreederPoolSize;
 		}
 
 		public ClassicProcessingScheme(
@@ -42,62 +36,15 @@ namespace Solve.ProcessingSchemes
 			ushort poolSize,
 			ushort maxLevelLosses = 5,
 			ushort maxLossesBeforeElimination = 30,
-			ushort reserveBreederPoolSize = 20)
-			: this(genomeFactory, (poolSize, poolSize, 2), maxLevelLosses, maxLossesBeforeElimination, reserveBreederPoolSize)
+			ushort championPoolSize = 20)
+			: this(genomeFactory, (poolSize, poolSize, 2), maxLevelLosses, maxLossesBeforeElimination, championPoolSize)
 		{
 		}
-
-		readonly IGenomeFactoryPriorityQueue<TGenome> FactoryReserve;
 
 		// First, and Minimum allow for tapering of pool size as generations progress.
 		public readonly (ushort First, ushort Minimum, ushort Step) PoolSize;
 		public readonly ushort MaxLevelLosses;
 		public readonly ushort MaxLossesBeforeElimination;
-		public readonly ushort ReserveBreederPoolSize;
-
-		readonly ConcurrentQueue<IGenomeFitness<TGenome, Fitness>> ReserveBreeders
-			= new ConcurrentQueue<IGenomeFitness<TGenome, Fitness>>();
-
-		Lazy<TGenome[]> ReadyReserve;
-
-		void AddReserved(IGenomeFitness<TGenome, Fitness> genome)
-		{
-			ReserveBreeders.Enqueue(genome);
-			if (ReadyReserve != null)
-				Interlocked.Exchange(ref ReadyReserve, null);
-			if (ReserveBreeders.Count > ReserveBreederPoolSize * 4)
-				SortAndReturnLimited(); // Overflowing?
-		}
-
-		TGenome[] SortAndReturnLimited()
-			=> LazyInitializer.EnsureInitialized(ref ReadyReserve, () => new Lazy<TGenome[]>(() =>
-			{
-				var result = ReserveBreeders.AsDequeueingEnumerable().Distinct().ToArray();
-				result.Sort(true);
-				var limited = result.Take(ReserveBreederPoolSize);
-				foreach (var e in limited) ReserveBreeders.Enqueue(e);
-				return limited.Select(g => g.Genome).ToArray();
-			})).Value;
-
-		bool ProduceFromReserve()
-		{
-			// Let it grow first...
-			if (ReserveBreeders.Count < ReserveBreederPoolSize)
-				return false;
-
-			var breeders = SortAndReturnLimited();
-			var len = breeders.Length;
-			for (var i = 0; i < len; i++)
-				FactoryReserve.Breed(breeders);
-
-			return len != 0;
-		}
-
-		protected override bool OnTowerBroadcast(Tower source, IGenomeFitness<TGenome, Fitness> genomeFitness)
-		{
-			AddReserved(genomeFitness);
-			return base.OnTowerBroadcast(source, genomeFitness);
-		}
 	}
 
 
--- Solve/ProcessingSchemes/ProcessingSchemeBase.cs
+++ Solve/ProcessingSchemes/ProcessingSchemeBase.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -11,21 +13,16 @@ namespace Solve.ProcessingSchemes
 		where TGenome : class, IGenome
 		where TTower : GenomeFitnessBroadcasterBase<TGenome>, IGenomeProcessor<TGenome>
 	{
-		protected ProcessingSchemeBase(IGenomeFactory<TGenome> genomeFactory)
+		protected ProcessingSchemeBase(IGenomeFactory<TGenome> genomeFactory, ushort championPoolSize)
 			: base(genomeFactory)
 		{
-
-		}
-
-		protected readonly ConcurrentDictionary<IProblem<TGenome>, TTower> Towers
-			= new ConcurrentDictionary<IProblem<TGenome>, TTower>();
-
-		protected abstract TTower NewTower(IProblem<TGenome> problem);
-
-		protected virtual bool OnTowerBroadcast(TTower source, IGenomeFitness<TGenome, Fitness> genomeFitness)
-		{
-			Factory[0].EnqueueChampion(genomeFitness.Genome);
-			return true;
+			ChampionPoolSize = championPoolSize;
+			if (championPoolSize != 0)
+			{
+				Champions = new ConcurrentQueue<IGenomeFitness<TGenome, Fitness>>();
+				FactoryReserve = genomeFactory[2];
+				FactoryReserve.ExternalProducers.Add(ProduceFromChampions);
+			}
 		}
 
 		public override void AddProblems(IEnumerable<IProblem<TGenome>> problems)
@@ -58,5 +55,96 @@ namespace Solve.ProcessingSchemes
 					CancellationToken = token,
 				}, Post);
 			});
+
+		#region Tower Managment
+		protected readonly ConcurrentDictionary<IProblem<TGenome>, TTower> Towers
+			= new ConcurrentDictionary<IProblem<TGenome>, TTower>();
+
+		protected abstract TTower NewTower(IProblem<TGenome> problem);
+
+		protected virtual bool OnTowerBroadcast(TTower source, IGenomeFitness<TGenome, Fitness> genomeFitness)
+		{
+			// This includes 'variations' and at least 1 mutation.
+			Factory[0].EnqueueChampion(genomeFitness.Genome);
+			AddChampion(genomeFitness);
+			return true;
+		}
+		#endregion
+
+		#region Champion Pool
+
+		public readonly ushort ChampionPoolSize;
+		protected readonly IGenomeFactoryPriorityQueue<TGenome> FactoryReserve;
+		readonly ConcurrentQueue<IGenomeFitness<TGenome, Fitness>> Champions;
+		Lazy<TGenome[]> RankedChampions;
+
+		protected void AddChampion(IGenomeFitness<TGenome, Fitness> genome)
+		{
+			Debug.Assert(genome != null);
+			if (Champions == null || genome == null) return;
+
+			Champions.Enqueue(genome);
+
+			// Queue has changed.  Flush the last result.
+			var rc = RankedChampions;
+			if (rc != null) Interlocked.CompareExchange(ref RankedChampions, null, rc);
+
+			var count = Champions.Count;
+			if (count > ChampionPoolSize * 100)
+			{
+				Debug.WriteLine($"Champion pool size reached: {count}");
+				GetRankedChampions(); // Overflowing?
+			}
+		}
+
+		TGenome[] GetRankedChampions()
+			=> LazyInitializer.EnsureInitialized(ref RankedChampions, () => new Lazy<TGenome[]>(() =>
+			{
+				var result = Champions
+					// Drain queue (some*)
+					.AsDequeueingEnumerable()
+					// Eliminate duplicates
+					.Distinct()
+					// Have enough to work with? (*)
+					.Take(ChampionPoolSize * 2)
+					// Setup ordering. Need to use snapshots for comparison.
+					.Select(e => (snapshot: e.Fitness.SnapShot(), genomeFitness: e))
+					// Higher sample counts are more valuable as they only arrive here as champions.
+					.OrderByDescending(e => e.snapshot.SampleCount)
+					.ThenBy(e => e.snapshot)
+					// Compile results.
+					.Select(e => e.genomeFitness)
+					.ToArray();
+
+#if DEBUG
+				((GenomeFactoryBase<TGenome>)Factory).MetricsCounter.Increment("Champion Ranking");
+#endif
+
+				// (.Take(ChampionPoolSize)) All champions deserve a chance, but we will only retain the ones that can fit in the pool.				
+				foreach (var e in result.Take(ChampionPoolSize)) Champions.Enqueue(e);
+				return result.Select(g => g.Genome).ToArray();
+			})).Value;
+
+		protected bool ProduceFromChampions()
+		{
+			if (Champions == null) return false;
+
+			var champions = GetRankedChampions();
+			var len = champions.Length;
+			if (champions.Length > 0)
+			{
+				FactoryReserve.EnqueueForMutation(champions[0]);
+				if (champions.Length > 1) FactoryReserve.Breed(champions);
+#if DEBUG
+				((GenomeFactoryBase<TGenome>)Factory).MetricsCounter.Increment("Champion Ranking Production");
+#endif
+				return true;
+			}
+
+			return false;
+		}
+
+		#endregion
+
 	}
 }
