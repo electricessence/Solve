9bfdcf82950ed8f72a12ee63a04d3047de20f494
Oren (electricessence) <oren@electrified.net>
2018-07-04 13:37:57 -0700

    Cleanup.

--- Solve/GenomeTestDelegate.cs
+++
@@ -1,8 +0,0 @@
-using System.Threading.Tasks;
-
-namespace Solve
-{
-	public delegate Task<IFitness>
-		GenomeTestDelegate<TGenome>(TGenome candidate, long sampleId = 0, bool mergeWithGlobal = false)
-		where TGenome : IGenome;
-}
