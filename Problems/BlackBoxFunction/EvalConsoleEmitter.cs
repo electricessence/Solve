b468752282edb6cb9f6423b5d7ce80efe3053704
electricessence <oren@electrified.net>
2018-07-15 22:09:23 -0700

    Full compilation.

---
+++ Problems/BlackBoxFunction/EvalConsoleEmitter.cs
@@ -0,0 +1,22 @@
+﻿using Open.Threading;
+using Solve;
+using Solve.Evaluation;
+using Solve.Experiment.Console;
+using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+
+namespace BlackBoxFunction
+{
+	public class EvalConsoleEmitter : ConsoleEmitterBase<EvalGenome>
+	{
+		public EvalConsoleEmitter(uint sampleMinimum = 50)
+			: base(sampleMinimum )
+		{
+
+		}
+	}
+}
