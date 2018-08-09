fd807cb1c62ec0c6893c6c90e4701614d364df56
Oren (electricessence) <oren@electrified.net>
2018-08-09 00:22:41 -0700

    Further consolidation to base classes.

--- Solve/ProcessingSchemes/Dataflow/DataflowScheme.Level.cs
+++ Solve/ProcessingSchemes/Dataflow/DataflowScheme.Level.cs
@@ -1,7 +1,6 @@
 ﻿using Open.Dataflow;
 using Open.Memory;
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -13,42 +12,20 @@ namespace Solve.ProcessingSchemes.Dataflow
 	// ReSharper disable once PossibleInfiniteInheritance
 	public partial class DataflowScheme<TGenome>
 	{
-		sealed class Level
+		sealed class Level : TowerLevelBase<TGenome, ProblemTower>
 		{
-			readonly ProblemTower Tower;
-			private readonly uint Index;
 			Level _nextLevel;
-
 			private Level NextLevel => LazyInitializer.EnsureInitialized(ref _nextLevel,
 				() => new Level(Index + 1, Tower));
 
 			public Level(
 				uint level,
 				ProblemTower tower,
-				byte priorityLevels = 3)
+				byte priorityLevels = 4)
+				: base(level, tower, priorityLevels)
 			{
-				Debug.Assert(level < tower.Environment.MaxLevels);
-
-				Index = level;
-				Tower = tower;
-				var env = Tower.Environment;
-				var factory = env.Factory[1];
-
-				var (First, Minimum, Step) = env.PoolSize;
-				var maxDelta = First - Minimum;
-				var decrement = Index * Step;
-				var poolSize = decrement > maxDelta ? Minimum : (ushort)(First - decrement);
-
-				Incomming = Enumerable
-					.Range(0, priorityLevels)
-					.Select(i => new ConcurrentQueue<(TGenome Genome, Fitness[] Fitness)>())
-					.ToArray();
-
-				Processor = new TransformBlock<(TGenome Genome, Fitness[] Fitness), LevelEntry<TGenome>>(
-					async c => LevelEntry.Merge(in c, (await Tower.Problem.ProcessSampleAsync(c.Genome, Index)).ToArray()));
-
-				var preselection
-					= new BatchBlock<LevelEntry<TGenome>>(poolSize);
+				Preselector
+					= new BatchBlock<LevelEntry<TGenome>>(PoolSize);
 
 				var selection
 					= new TransformBlock<LevelEntry<TGenome>[], LevelEntry<TGenome>[][]>(pool =>
@@ -56,29 +33,23 @@ namespace Solve.ProcessingSchemes.Dataflow
 							.Select((p, i) => pool.OrderBy(e => e.Scores[i], ArrayComparer<double>.Descending).ToArray())
 							.ToArray());
 
-				Processor.LinkTo(preselection);
-				preselection.LinkTo(selection);
+				Preselector.LinkTo(selection);
 				selection.LinkTo(pools =>
 				{
 					var poolCount = pools.Length;
-					var midPoint = poolSize / 2;
+					var midPoint = PoolSize / 2;
 					var promoted = new HashSet<string>();
 
 					var isTop = _nextLevel == null;
 					// Place champions first.
 					{
-						var problemPools = Tower.Problem.Pools;
-						for (var p = 0; p < poolCount; ++p)
+						for (byte p = 0; p < poolCount; ++p)
 						{
 							var gf = pools[p][0].GenomeFitness;
 							if (isTop)
-							{
-								problemPools[p].Champions?.Add(gf.Genome, gf.Fitness[p]);
-								factory.EnqueueChampion(gf.Genome); // Okay to enqueue more than once if the same since it may be more significant.
-								Tower.Broadcast(gf, p);
-							}
+								ProcessChampion(p, gf);
 							if (promoted.Add(gf.Genome.Hash))
-								NextLevel.Post(0, gf);
+								PostNextLevel(1, gf);
 						}
 					}
 
@@ -89,12 +60,12 @@ namespace Solve.ProcessingSchemes.Dataflow
 						{
 							var gf = pools[p][i].GenomeFitness;
 							if (promoted.Add(gf.Genome.Hash))
-								NextLevel.Post(1, gf);
+								PostNextLevel(2, gf);
 						}
 					}
 
 					// Make sure losers have thier rejection count incremented.
-					for (var i = midPoint; i < poolSize; ++i)
+					for (var i = midPoint; i < PoolSize; ++i)
 					{
 						for (var p = 0; p < poolCount; ++p)
 						{
@@ -105,7 +76,7 @@ namespace Solve.ProcessingSchemes.Dataflow
 					// Distribute losers either back into the pool, pass them to the next level, or let them disapear (rejected).
 					var maxLoses = Tower.Environment.MaxLevelLosses;
 					var maxRejection = Tower.Environment.MaxLossesBeforeElimination;
-					for (var i = midPoint; i < poolSize; ++i)
+					for (var i = midPoint; i < PoolSize; ++i)
 					{
 						for (var p = 0; p < poolCount; ++p)
 						{
@@ -119,45 +90,36 @@ namespace Solve.ProcessingSchemes.Dataflow
 							{
 								var fitnesses = gf.Fitness;
 								if (fitnesses.Any(f => f.RejectionCount < maxRejection))
-									NextLevel.Post(2, gf);
+									PostNextLevel(3, gf);
 								else if (Tower.Environment.Factory is GenomeFactoryBase<TGenome> f)
 									f.MetricsCounter.Increment("Genome Rejected");
 							}
 							else
 							{
 								Debug.Assert(loser.LevelLossRecord > 0);
-								preselection.Post(loser); // Didn't win, but still in the game.
+								Preselector.Post(loser); // Didn't win, but still in the game.
 							}
 
 						}
 					}
 				});
+
+
 			}
 
-			readonly ConcurrentQueue<(TGenome Genome, Fitness[] Fitness)>[] Incomming;
-			readonly TransformBlock<(TGenome Genome, Fitness[] Fitness), LevelEntry<TGenome>> Processor;
+			readonly BatchBlock<LevelEntry<TGenome>> Preselector;
 
-			public void Post(int priority, (TGenome Genome, Fitness[] Fitness) challenger)
+			protected override void PostNextLevel(byte priority, (TGenome Genome, Fitness[] Fitness) challenger)
+				=> NextLevel.Post(priority, challenger);
+
+			protected override void ProcessInjested(byte priority, (TGenome Genome, Fitness[] Fitness) challenger)
 			{
-				Incomming[priority].Enqueue(challenger);
-
-				int count;
-				do
+				ProcessEntry(challenger).ContinueWith(e =>
 				{
-					count = 0;
-					var len = Incomming.Length;
-					for (var i = 0; i < len; i++)
-					{
-						var q = Incomming[i];
-						if (!q.TryDequeue(out var c)) continue;
-
-						if (!Processor.Post(c))
-							throw new Exception("Processor refused challenger.");
-						i = -1; // Reset to top queue.
-						++count;
-					}
-				}
-				while (count != 0);
+					var result = e.Result;
+					if (result != null && !Preselector.Post(result))
+						throw new Exception("Processor refused challenger.");
+				});
 			}
 
 		}
