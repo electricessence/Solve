71a0f75007d38385e4c3e8fe49426de3ce77c652
Oren (electricessence) <oren@electrified.net>
2020-04-16 00:48:32 -0700

    More tweaks.

--- Problems/Eater/Core/GenomeFactory.Variations.cs
+++ Problems/Eater/Core/GenomeFactory.Variations.cs
@@ -8,8 +8,6 @@ namespace Eater
 {
 	public partial class GenomeFactory
 	{
-		static readonly IEnumerable<Step> ForwardOne = StepCount.Forward(1);
-
 		public static IEnumerable<IEnumerable<Step>> GetVariations(IReadOnlyList<Step> source)
 		{
 			// Cut lenghts in half.
@@ -18,10 +16,14 @@ namespace Eater
 			foreach (var i in Enumerable.Range(0, stepCount).Shuffle())
 			{
 				var step = stepCounts[i];
-
-				if (step.Step != Step.Forward) continue;
 				var head = stepCounts.Take(i).Steps();
 				var tail = stepCounts.Skip(i + 1).Steps();
+
+				// Remove one.
+				yield return head
+					.Concat(tail);
+
+				if (step.Step != Step.Forward) continue;
 
 				// Double a length.
 				yield return head
@@ -39,6 +41,11 @@ namespace Eater
 				yield return head
 					.Concat(StepCount.Forward(step.Count + 1))
 					.Concat(tail);
+
+				// Add one.
+				yield return head
+					.Concat(StepCount.Forward(step.Count - 1))
+					.Concat(tail);
 			}
 
 			yield return source.Reverse();
@@ -46,51 +53,25 @@ namespace Eater
 			// Pattern is doubled.
 			yield return Enumerable.Repeat(source, 2).SelectMany(s => s);
 
+			// All forward movement lengths reduced by 1.
+			yield return stepCounts.Select(sc => sc.Step == Step.Forward && sc.Count > 1 ? --sc : sc).Steps();
+
+			// All forward movement lengths doubled...
+			yield return source.SelectMany(g => Enumerable.Repeat(g, g == Step.Forward ? 2 : 1));
+
 			var len = source.Count;
-			var lenMinusOne = len - 1;
-			//if (lenMinusOne > 1)
-			//{
-			//	var removeTail = source.Take(lenMinusOne);
-			//	yield return removeTail.TrimTurns();
+			var half = len / 2;
+			if (half <= 2) yield break;
+			yield return source.Take(half);
+			yield return source.Skip(half);
 
-			//	var removeHead = source.Skip(1);
-			//	yield return removeHead.TrimTurns();
-			//}
-
-			////yield return ForwardOne.Concat(source);
-			////yield return source.Concat(ForwardOne);
-
-			//// All forward movement lengths reduced by 1.
-			//yield return stepCounts.Select(sc => sc.Step == Step.Forward && sc.Count > 1 ? --sc : sc).Steps();
-
-			//// Remove step at every point.
-			//if (lenMinusOne > 2)
-			//{
-			//	for (var i = 1; i < len - 2; i++)
-			//	{
-			//		var head = source.Take(i);
-			//		var tail = source.Skip(i + 1);
-			//		yield return head.Concat(tail);
-			//	}
-			//}
-
-			//// All forward movement lengths doubled...
-			//yield return source.SelectMany(g => Enumerable.Repeat(g, g == Step.Forward ? 2 : 1));
-
-
-
-			//var half = len / 2;
-			//if (half <= 2) yield break;
-			//yield return source.Take(half);
-			//yield return source.Skip(half);
-
-			//var third = len / 3;
-			//if (third <= 2) yield break;
-			//yield return source.Take(third);
-			//yield return source.Skip(third).Take(third);
-			//yield return source.Skip(third);
-			//yield return source.Take(2 * third);
-			//yield return source.Skip(2 * third);
+			var third = len / 3;
+			if (third <= 2) yield break;
+			yield return source.Take(third);
+			yield return source.Skip(third).Take(third);
+			yield return source.Skip(third);
+			yield return source.Take(2 * third);
+			yield return source.Skip(2 * third);
 		}
 
 		protected override IEnumerable<Genome> GetVariationsInternal(Genome source)
